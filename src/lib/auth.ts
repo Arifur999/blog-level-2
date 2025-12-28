321534b487416e740de0a6b62d0a0dc03880a23e
Arifur999 <arr96777@gmail.com>
2025-12-28 22:29:56 +0600

    connect email to nodemailer

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,7 +1,22 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import { prisma } from "./prisma";
+const nodemailer = require("nodemailer");
 // If your Prisma file is located elsewhere, you can change the path
+
+
+
+
+const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false, // Use true for port 465, false for port 587
+  auth: {
+    user: process.env.GM_ID,
+    pass: process.env.GM_PASS,
+  },
+});
+
 
 export const auth = betterAuth({
   database: prismaAdapter(prisma, {
@@ -35,7 +50,19 @@ export const auth = betterAuth({
     },
 emailVerification: {
     sendVerificationEmail: async ( { user, url, token }, request) => {
+
+
+        const verificationUrl = `${process.env.APP_URL}/verify-email?token=${token}&email=${encodeURIComponent(user.email)}`;
       console.log(`Send verification email to ${user.email} with URL: ${url} and token: ${token}`);
+    
+     const info = await transporter.sendMail({
+    from: '"Maddison Foo Koch" <maddison53@ethereal.email>',
+    to: "arifur.rahman.we@gmail.com",
+    subject: "Hello ✔",
+    text: "Hello world?", // Plain-text version of the message
+    html: "<b>Hello world?</b>", // HTML version of the message
+  });
+    
     },
   },
 });
