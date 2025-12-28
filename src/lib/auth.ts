e9d194e35a20519e64b89a1f598de13448745271
Arifur999 <arr96777@gmail.com>
2025-12-28 21:20:03 +0600

    added better auth email api

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -3,15 +3,30 @@ import { prismaAdapter } from "better-auth/adapters/prisma";
 import { prisma } from "./prisma";
 // If your Prisma file is located elsewhere, you can change the path
 
-
 export const auth = betterAuth({
-    database: prismaAdapter(prisma, {
-        provider: "postgresql", // or "mysql", "postgresql", ...etc
-    }),
-
-    
-  emailAndPassword: { 
-    enabled: true, 
-  }, 
-
+  database: prismaAdapter(prisma, {
+    provider: "postgresql", // or "mysql", "postgresql", ...etc
+  }),
+  trustedOrigins: [process.env.APP_URL || "http://localhost:3000"],
+  user: {
+    additionalFields: {
+      role: {
+        type: "string",
+        defaultValue: "user",
+        required: false,
+      },
+      phone: {
+        type: "string",
+        required: false,
+      },
+      status: {
+        type: "string",
+        defaultValue: "active",
+        required: false,
+      },
+    },
+    emailAndPassword: {
+      enabled: true,
+    },
+  },
 });
