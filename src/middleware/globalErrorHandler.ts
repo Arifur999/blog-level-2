7403459b52c9b9e28551d80f01ad98c32c6e0e0b
Arifur999 <arr96777@gmail.com>
2026-01-09 22:27:39 +0600

    PrismaClientValidationError

--- src/middleware/globalErrorHandler.ts
+++ src/middleware/globalErrorHandler.ts
@@ -1,12 +1,27 @@
-import { NextFunction, Request, Response } from "express";
+import e, { NextFunction, Request, Response } from "express";
+import { Prisma } from "../../generated/prisma/client";
 
 function errorHandler(
     err: any,
      req: Request, 
      res: Response, 
      next: NextFunction) {
+
+let statusCode = 500
+let message = "Internal Server Error"
+let error = err;
+
+//prismaClient Validation error handling
+if (err instanceof Prisma.PrismaClientValidationError) {
+    statusCode = 400;
+    message = "Validation Error";
+    error = err.message;
+} 
+
+res.status(statusCode).json({ message, error });
+
   console.error(err.stack);
-  res.status(500)
+  res.status(statusCode)
   res.json({ message: "Internal Server Error", error: err.message });
 }
 export default errorHandler;
